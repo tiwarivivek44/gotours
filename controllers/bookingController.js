36eefdfbc6ab24d351df3da748ae7b165aef97d3
Vivek Tiwari <tiwarivivek44@gmail.com>
2022-09-10 04:39:31 +0530

    Update bookingController.js

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -72,7 +72,7 @@ const createBookingCheckout = async session => {
 };
 
 exports.webhookCheckout = (req, res, next) => {
-  const signature = req.header['stripe-signature'];
+  const signature = req.headers['stripe-signature'];
   let event;
   try {
     event = stripe.webhooks.constructEvent(
